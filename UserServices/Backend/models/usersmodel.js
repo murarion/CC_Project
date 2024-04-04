491fdc5035c56b8c784c43da6cd916817b9a72f1
Kashish Patel <kashishpatel1601@gmail.com>
2024-04-04 23:24:44 +0530

    Kashish

---
+++ UserServices/Backend/models/usersmodel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+	name: { type : String, required:true},
+    mailid: { type : String, required:true},
+    phno: { type : String, required : true},
+    password:{ type : String, required:true},
+
+})
+
+module.exports = new mongoose.model("users", userSchema);
---
+++ UserServices/Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Home from './components/Home'
+//  Assuming you have a Register component
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
